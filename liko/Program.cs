5d870b0648ec81975beeb81ee6780d643d7ccb54
LikoKuridze <lkuridze@credo.ge>
2024-10-23 13:01:49 +0400

    Add project files.

---
+++ liko/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string name = "Liko";
+int age = 30;
+bool isMarried = true;
+double height = 169.5;
+float weight = 55.7f;
+bool isemployed = true;
+char initial = 'L';
+
+
+
+Console.WriteLine($"Hello, My name is {name}, my age is: {age},my height is {height},my weight is {weight},my initcial is {initial}, ismarried{isMarried},isemployed {isemployed}");
